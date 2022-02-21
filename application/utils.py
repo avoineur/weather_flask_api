d4bda3a25cce459cb40a31cef40ef8e907bd5021
Boris Skrobek <boris.skrobek@autovisual.com>
2022-02-21 13:21:39 +0100

    add comments in utils.py

--- application/utils.py
+++ application/utils.py
@@ -4,6 +4,11 @@ import requests
 
 
 def check_input(args: dict) -> dict:
+    """
+    Check if user entries latitude and longitude are well formated. If ok, retruns a dict with 
+    lat and lng converted as flaots
+        - args: dict. request.args
+    """
     lat = args.get("lat")
     lng = args.get("lng")
     if lat is None:
@@ -13,13 +18,25 @@ def check_input(args: dict) -> dict:
     return {"lat": check_lat_lng(lat, "latitude"), "lng": check_lat_lng(lng, "longitude")}
 
 
-def lat_lng_error_message(value: str, param: str):
+def lat_lng_error_message(value: str, param: str) -> str:
+    """
+    Defines an error message when lat or long are not in good format
+        - value: str. Latitude or longitude value as a string
+        - param: str. Name of the param (either latitude or longitude)
+    """    
     error_msg = "Incorrect " + param + " parameter: " + value + ". Latitudes should be floats"
     error_msg += " in the range (-90, 90) and longitudes should be floats in the range (-180, 180)"
     return error_msg
 
 
 def check_lat_lng(value: str, param: str) -> float:
+    """
+    Check if user entries latitude and longitude are well formated. If ok, returns the value as 
+    a float.
+        - value: str. Latitude or longitude value as a string
+        - param: str. Name of the param (either latitude or longitude)
+    """
+
     try:
         value = float(value)
     except ValueError:
@@ -37,6 +54,14 @@ def check_lat_lng(value: str, param: str) -> float:
 
 def get_location_id(lat: float, lng: float, accu_weather_api_key: str, 
                     accu_weather_api_url: str) -> str:
+    """ 
+    Calls Accuweather API location endpoint to get closest city to lat and lng
+        - lat: float. Latitude of the location the user if searching for
+        - lng: float. Longitude of the location the user if searching for
+        - accu_weather_api_key: str. Accuweather API key you get after registration. See 
+        https://developer.accuweather.com/ for further details
+        - accu_weather_api_url: str. Accuweather API base url.
+    """
     req_url = accu_weather_api_url + "/locations/v1/cities/geoposition/search"
     params = {"apikey": accu_weather_api_key, "q": str(lat) + "," + str(lng)}
     response = requests.get(req_url, params=params)
@@ -48,6 +73,14 @@ def get_location_id(lat: float, lng: float, accu_weather_api_key: str,
 
 def get_current_conditions(location_id: str, accu_weather_api_key: str,
                            accu_weather_api_url: str) -> dict:
+    """ 
+    Calls Accuweather API location endpoint to get conditions in the location defined by 
+    location_id.
+        - location_id: str. As returned by get_location_id
+        - accu_weather_api_key: str. Accuweather API key you get after registration. See 
+        https://developer.accuweather.com/ for further details
+        - accu_weather_api_url: str. Accuweather API base url.
+    """
     req_url = accu_weather_api_url + "/currentconditions/v1/" + location_id
     params = {"apikey": accu_weather_api_key, "details": "true"}
     response = requests.get(req_url, params=params)
@@ -64,6 +97,15 @@ def get_current_conditions(location_id: str, accu_weather_api_key: str,
 
 def get_weather(lat: float, lng: float, accu_weather_api_key: str,
                 accu_weather_api_url: str) -> dict:
+    """ 
+    Get weather conditions in the closest city to lat and lng through two Accuweather API calls 
+    (location, and current_conditions)
+        - lat: float. Latitude of the location the user if searching for
+        - lng: float. Longitude of the location the user if searching for
+        - accu_weather_api_key: str. Accuweather API key you get after registration. See 
+        https://developer.accuweather.com/ for further details
+        - accu_weather_api_url: str. Accuweather API base url.
+    """
     location_id = get_location_id(lat, lng, accu_weather_api_key, accu_weather_api_url)
     res = get_current_conditions(location_id, accu_weather_api_key, accu_weather_api_url)
     return res
